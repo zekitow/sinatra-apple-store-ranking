a0279d96710dce968ec1bdb7a589db1cc154e109
Jose Ribeiro Filho <zekitow@gmail.com>
2016-03-18 11:21:10 -0300

    Validation required attributes on request.

--- app/controllers/apple_store_controller.rb
+++ app/controllers/apple_store_controller.rb
@@ -4,7 +4,8 @@ class AppleStoreController < ApplicationController
   # Search all ranking (free, grossing and paid)
   #
   get '/apple-store/by-ranking' do
-    result          = AppleStoreRanking.new.find_by_genre(api_request(params))
+    api_request     = by_ranking_request(params)
+    result          = AppleStoreRanking.new.find_by_genre(api_request)
     aditional_infos = AppleStoreLookup.new.find_all(map_lookup_ids(result))
 
     json(aggregate_element_lists(result, aditional_infos))
@@ -14,8 +15,9 @@ class AppleStoreController < ApplicationController
   # Search a ranking by specific category (free, grossing or paid)
   #
   get '/apple-store/category' do
+    api_request     = by_category_request(params)
 
-    result          = AppleStoreRanking.new.find_by_genre_and_monetization_type(api_request(params))
+    result          = AppleStoreRanking.new.find_by_genre_and_monetization_type(api_request)
     aditional_infos = AppleStoreLookup.new.find_all(result['adamIds'])
 
     json(aggregate_elements(result, aditional_infos))
@@ -23,7 +25,19 @@ class AppleStoreController < ApplicationController
 
 
   private
-    def api_request(params)
+    def by_ranking_request(params)
+      unless params[:genreId].present?
+        halt 422, json({ message: 'genreId is required.' })
+      end
+
+      { genre_id: params[:genreId] }
+    end
+
+    def by_category_request(params)
+      unless params[:genreId].present? or params[:monetization_type].present?
+        halt 422, json({ message: 'Missing parameters, provide genreId and monetizationType.' })
+      end
+
       {
         genre_id: params[:genreId],
         monetization_type: params[:monetizationType]
