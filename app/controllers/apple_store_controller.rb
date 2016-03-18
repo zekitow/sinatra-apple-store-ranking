bc46f8b809a174a1ed97f11b7cc537fb2cf93195
Jose Ribeiro Filho <zekitow@gmail.com>
2016-03-18 10:24:45 -0300

    Spliting endpoints.

--- app/controllers/apple_store_controller.rb
+++ app/controllers/apple_store_controller.rb
@@ -1,13 +1,18 @@
 class AppleStoreController < ApplicationController
 
-  get '/apple-store/ranking' do
-    result = AppleStoreRanking.new.find_all(params[:categoryId])
+  get '/apple-store/by-ranking' do
+    result = AppleStoreRanking.new.find_all
+    result = result['topCharts']
 
-    if params[:categoryId].present?
-      result = result['selectedChart']
-    else
-      result = result['topCharts']
-    end
+    aditional_infos = AppleStoreLookup.new.find_all(map_lookup_ids(result))
+
+    json(aggregate(result, aditional_infos))
+  end
+
+  get '/apple-store/category/:categoryId' do
+
+    result = AppleStoreRanking.new.find_all(params[:categoryId])
+    result = result['selectedChart']
 
     aditional_infos = AppleStoreLookup.new.find_all(map_lookup_ids(result))
 
